2e1c48e6d06e19368e0f58b433a74512b5e95f9b
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2021-01-06 23:09:45 -0300

    feat: add hero component

---
+++ src/components/hero/HeroSection.vue
@@ -0,0 +1,122 @@
+<template>
+  <section class="hero container">
+    <div class="hero-content">
+      <h1 class="hero__headline">I'm Jeferson Brito</h1>
+      <h2 class="hero__subheadline">Web Developer & UI Designer</h2>
+      <SocialMediaIcons />
+    </div>
+    <aside>
+      <ScrollIndicator class="is-mobile-only" />
+      <div class="hero-avatar">
+        <g-image
+          src="@/assets/img/me.jpg"
+          alt="me"
+          class="hero-avatar__image"
+          quality="100"
+          width="140"
+        />
+      </div>
+    </aside>
+  </section>
+</template>
+
+<script>
+import ScrollIndicator from '@/components/ui/ScrollIndicator.vue'
+import SocialMediaIcons from '@/components/social-media/SocialMediaIcons.vue'
+
+export default {
+  components: {
+    ScrollIndicator,
+    SocialMediaIcons,
+  },
+}
+</script>
+
+<style scoped>
+.hero {
+  display: grid;
+  grid-template-columns: minmax(50%, 700px) auto;
+  align-items: center;
+  padding: 3rem 0;
+}
+
+.hero-content {
+  font-family: var(--font-title);
+}
+
+.hero__headline {
+  font-size: var(--text-3xl);
+  font-size: clamp(var(--text-xl), 1vw + var(--text-2xl), var(--text-3xl));
+  font-weight: 700;
+}
+
+.hero__subheadline {
+  font-size: var(--text-2xl);
+  font-size: clamp(var(--text-lg), 1vw + var(--text-xl), var(--text-2xl));
+  color: var(--color-default-white);
+  opacity: 0.35;
+  letter-spacing: 1.2px;
+  margin-bottom: 20px;
+}
+
+aside {
+  justify-self: end;
+  padding: 0 17px;
+}
+
+aside .hero-avatar {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 323px;
+  height: 275px;
+  background-color: var(--color-black-800);
+  border-radius: var(--radius-default);
+  box-shadow: 1px 4px 4px 3px rgba(0, 0, 0, 0.14);
+}
+
+aside .hero-avatar .hero-avatar__image {
+  clip-path: circle(70px at center);
+}
+
+@media screen and (max-width: 425px) {
+  .hero {
+    height: 100vh;
+    grid-template-columns: 1fr;
+  }
+
+  aside .hero-avatar {
+    display: none;
+  }
+
+  .hero-content {
+    text-align: center;
+  }
+
+  .hero__headline {
+    font-size: calc(var(--text-xl) + 0.75rem);
+    letter-spacing: 1.2px;
+  }
+
+  .hero__subheadline {
+    font-weight: 500;
+    letter-spacing: initial;
+    font-size: var(--text-base);
+  }
+
+  .social-media-icons a {
+    padding: 8px;
+  }
+
+  .social-media-icons a svg {
+    max-width: 35px;
+  }
+}
+
+@media screen and (max-width: 1024px) {
+  .hero {
+    padding-right: 18px;
+    padding-left: 18px;
+  }
+}
+</style>
