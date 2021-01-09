917cb6cc3ff0a401c58e3375ba55ee3f9d1b22dd
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2021-01-09 16:38:37 -0300

    feat: add PostsList and Preview component

---
+++ src/components/posts/PostPreview.vue
@@ -0,0 +1,50 @@
+<template>
+  <article class="post-preview">
+    <time class="post-preview__date" :datetime="date.toISOString()">
+      {{ formattedDate }}
+    </time>
+    <h4 class="post-preview__title">{{ title }}</h4>
+  </article>
+</template>
+
+<script>
+export default {
+  props: {
+    date: {
+      type: Date,
+    },
+    title: {
+      type: String,
+    },
+  },
+  computed: {
+    formattedDate() {
+      return this.date.toLocaleString(['en-US'], {
+        day: '2-digit',
+        month: 'short',
+      })
+    },
+  },
+}
+</script>
+
+<style scoped>
+.post-preview {
+  background-color: var(--color-black-800);
+  border-radius: var(--radius-default);
+  width: 100%;
+  padding: 18px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  margin-bottom: 28px;
+  display: flex;
+}
+
+.post-preview__date {
+  flex-basis: 60px;
+  opacity: 0.7;
+}
+
+.post-preview__title {
+  margin: 0;
+}
+</style>
---
+++ src/components/posts/PostsList.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="posts-list container">
+    <h3>Latest Posts</h3>
+    <PostPreview
+      v-for="{ title, created_at } in dummyPosts"
+      :key="title"
+      :title="title"
+      :date="created_at"
+    />
+  </div>
+</template>
+
+<script>
+import PostPreview from './PostPreview.vue'
+
+export default {
+  components: {
+    PostPreview,
+  },
+  data() {
+    return {
+      dummyPosts: [
+        { title: 'What is CSS Houdini?', created_at: new Date() },
+        {
+          title: 'Awesome DevTools features',
+          created_at: new Date(2020, 12, 22),
+        },
+        {
+          title: 'Vue 3.0 is finally released!',
+          created_at: new Date(2020, 10, 25),
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style scoped>
+.posts-list {
+  display: flex;
+  flex-flow: row wrap;
+}
+
+.posts-list h3 {
+  width: 100%;
+  font-family: var(--font-title);
+  text-transform: uppercase;
+  margin-bottom: 33px;
+}
+
+@media screen and (max-width: 1024px) {
+  .posts-list {
+    padding: 18px;
+  }
+}
+</style>
