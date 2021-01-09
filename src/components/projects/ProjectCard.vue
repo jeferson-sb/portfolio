9f41a3b261fe2b5ed514c48bdab4fd8e03c944f9
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2021-01-09 16:39:44 -0300

    feat: add ProjectCard and Grid

---
+++ src/components/projects/ProjectCard.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="project-card">
+    <figure class="project-thumbnail">
+      <g-image src="https://picsum.photos/1280/720" alt="project 0101" />
+    </figure>
+    <div class="project-card__content">
+      <div class="project-card__header">
+        <h4>Project 0101</h4>
+        <p>
+          Lorem ipsum dolor sit amet consectetur.
+        </p>
+      </div>
+      <div class="project-card__footer">
+        <TagGroup :tags="['frontend', 'ai', 'react']" />
+        <g-link to="https://github.com/jeferson-sb" class="button">
+          Github
+        </g-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import TagGroup from '@/components/ui/TagGroup.vue'
+
+export default {
+  components: {
+    TagGroup
+  }
+}
+</script>
+
+<style scoped>
+
+.project-card {
+  display: flex;
+}
+
+.project-thumbnail {
+  flex-basis: 60%;
+  border-radius: var(--radius-default) 0 0 var(--radius-default);
+}
+
+.project-thumbnail img {
+  border-radius: inherit;
+  object-fit: cover;
+  min-height: 100%;
+}
+
+.project-card .project-card__content {
+  background-color: var(--color-black-800);
+  padding: 18px;
+  border-radius: 0 var(--radius-default) var(--radius-default) 0;
+}
+
+.project-card__header {
+  margin-bottom: 20px;
+}
+
+.project-card .project-card__header h4 {
+  font-size: 18px;
+  margin-bottom: 10px;
+}
+
+.project-card .project-card__header p {
+  opacity: 0.7;
+}
+
+.project-card .project-card__footer .button {
+  --button-bg-color: var(--color-primary);
+
+  border-radius: var(--radius-default);
+  width: 100%;
+  text-decoration: none;
+  font-weight: 700;
+  color: var(--color-default-black);
+}
+
+@media screen and (max-width: 425px) {
+  .project-card {
+    flex-direction: column;
+  }
+
+  .project-thumbnail {
+    max-height: 180px;
+  }
+}
+
+</style>
---
+++ src/components/projects/ProjectsGrid.vue
@@ -0,0 +1,52 @@
+<template>
+  <section class="projects-grid container">
+    <h3>Projects</h3>
+    <ProjectCard />
+    <ProjectCard />
+    <ProjectCard />
+    <ProjectCard />
+  </section>
+</template>
+
+<script>
+import ProjectCard from './ProjectCard'
+
+export default {
+  components: {
+    ProjectCard,
+  },
+}
+</script>
+
+<style scoped>
+.projects-grid {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 2rem;
+  margin-bottom: 4rem;
+}
+
+.projects-grid h3 {
+  grid-column: 1 / 3;
+  font-family: var(--font-title);
+  text-transform: uppercase;
+  margin: 33px 0 0;
+}
+
+@media screen and (max-width: 1024px) {
+  .projects-grid {
+    padding: 18px;
+  }
+}
+
+@media screen and (max-width: 425px) {
+  .projects-grid {
+    grid-template-columns: 1fr;
+  }
+
+  .projects-grid h3 {
+    grid-column: 1;
+  }
+}
+
+</style>
