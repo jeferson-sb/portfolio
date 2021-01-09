dba996f3741719e727d895143c546771bfebd362
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2021-01-09 16:40:38 -0300

    feat: add PostsList and ProjectsGrid to Index page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,15 +1,21 @@
 <template>
   <Layout>
     <HeroSection />
+    <PostsList />
+    <ProjectsGrid />
   </Layout>
 </template>
 
 <script>
 import HeroSection from '@/components/hero/HeroSection.vue'
+import PostsList from '@/components/posts/PostsList.vue'
+import ProjectsGrid from '@/components/projects/ProjectsGrid.vue'
 
 export default {
   components: {
     HeroSection,
+    PostsList,
+    ProjectsGrid,
   },
 }
 </script>
