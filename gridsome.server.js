2c7903e14aaec8808ee4bdf3fb0a491ae7d8f33c
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2021-01-10 16:09:17 -0300

    feat: add gridsome.server.js

---
+++ gridsome.server.js
@@ -0,0 +1,9 @@
+module.exports = function (api) {
+  api.loadSource(({ addCollection }) => {
+    // Use the Data Store API here: https://gridsome.org/docs/data-store-api/
+  })
+
+  api.createPages(({ createPage }) => {
+    // Use the Pages API here: https://gridsome.org/docs/pages-api/
+  })
+}
