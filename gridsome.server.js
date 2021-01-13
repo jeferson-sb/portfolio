c1bd2d85e05f74ca26f0b9be57a431c055c351c6
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2021-01-13 20:52:45 -0300

    feat: add dev.to posts collection

--- gridsome.server.js
+++ gridsome.server.js
@@ -1,6 +1,21 @@
+const axios = require('axios')
+
 module.exports = function (api) {
-  api.loadSource(({ addCollection }) => {
-    // Use the Data Store API here: https://gridsome.org/docs/data-store-api/
+  api.loadSource(async ({ addCollection }) => {
+    const { data: articles } = await axios(
+      'https://dev.to/api/articles/me/published',
+      {
+        headers: {
+          'api-key': process.env.DEVTO_API_KEY,
+        },
+      }
+    )
+
+    const collection = addCollection({
+      typeName: 'Articles',
+    })
+
+    articles.forEach((article) => collection.addNode(article))
   })
 
   api.createPages(({ createPage }) => {
