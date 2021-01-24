ca657275e5c4f8b1f9acbe431ae7f4a177e59d08
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2021-01-24 18:16:44 -0300

    fix: 16x16 and 32x32 favicon links

--- src/main.js
+++ src/main.js
@@ -6,16 +6,6 @@ import '~/assets/styles/global.css'
 export default (Vue, { appOptions, head }) => {
   Vue.component('Layout', DefaultLayout)
 
-  head.link.push({
-    rel: 'icon',
-    sizes: '16',
-    href: '@/assets/icons/favicon-16x16.png',
-  })
-  head.link.push({
-    rel: 'icon',
-    sizes: '32',
-    href: '@/assets/icons/favicon-32x32.png',
-  })
   head.meta.push({
     name: 'google-site-verification',
     content: process.env.GOOGLE_SEARCH_CONSOLE_TOKEN,
