5b4840353eab1a76e4e4db1a582f88f89cc5faa5
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2021-02-01 15:35:37 -0300

    fix: analytics script to be production-only

--- src/main.js
+++ src/main.js
@@ -6,6 +6,17 @@ import '~/assets/styles/global.css'
 export default (Vue, { appOptions, head }) => {
   Vue.component('Layout', DefaultLayout)
 
+  // Production-only
+  if (process.env.NODE_ENV === 'production') {
+    head.script.push({
+      src: process.env.ANALYTICS_URL,
+      async: true,
+      defer: true,
+      'data-cache': true,
+      'data-website-id': process.env.ANALYTICS_WEBSITE_ID,
+    })
+  }
+
   head.meta.push({
     name: 'google-site-verification',
     content: process.env.GOOGLE_SEARCH_CONSOLE_TOKEN,
