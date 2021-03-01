290014053e186670406b51063ef10f21ed0e73c9
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2021-03-01 15:28:44 -0300

    feat: add github repository component

---
+++ src/components/github/GithubProjects.vue
@@ -0,0 +1,80 @@
+<template>
+  <section class="github-projects container">
+    <h3>Open Source</h3>
+    <h4>Latest contributions</h4>
+    <div class="github-repositories">
+      <GithubRepoCard
+        v-for="repo in repositories"
+        :key="repo.id"
+        :repository="repo"
+      />
+    </div>
+  </section>
+</template>
+
+<script>
+import GithubRepoCard from './GithubRepoCard.vue'
+
+export default {
+  components: {
+    GithubRepoCard,
+  },
+  data() {
+    return {
+      repositories: [
+        {
+          id: 1,
+          name: 'Grommet',
+          description:
+            'a react-based framework that provides accessibility, modularity, responsiveness, and theming in a tidy package',
+          language: 'JavaScript',
+          stars: 7000,
+          prs: 245,
+        },
+        {
+          id: 2,
+          name: 'Grommet',
+          description:
+            'a react-based framework that provides accessibility, modularity, responsiveness, and theming in a tidy package',
+          language: 'JavaScript',
+          stars: 7000,
+          prs: 245,
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style scoped>
+.github-projects,
+.github-repositories {
+  flex-wrap: wrap;
+}
+
+.github-projects h3,
+.github-projects h4 {
+  text-transform: uppercase;
+}
+
+.github-projects h3 {
+  flex-basis: 100%;
+}
+
+.github-projects h4 {
+  color: var(--color-gray-400);
+  font-size: var(--text-sm);
+  margin: 10px 0 15px 0;
+}
+
+.github-repositories {
+  display: flex;
+  justify-content: center;
+}
+
+@media screen and (max-width: 1024px) {
+  .github-projects {
+    padding: 18px;
+  }
+}
+</style>
---
+++ src/components/github/GithubRepoCard.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="github-repo-card">
+    <h4 class="github-repo-card__title">
+      <a href="https://github.com">{{ repository.name }}</a>
+    </h4>
+    <p class="github-repo-card__description">{{ repository.description }}</p>
+    <p class="github-repo-details">
+      <span class="github-repo-language">
+        <CircleFillSVG />
+        <span>{{ repository.language }}</span>
+      </span>
+      <span class="github-repo-stars">
+        <StarFilledSVG />
+        <span>{{ repository.stars }}</span>
+      </span>
+      <span class="github-repo-prs">
+        <PullRequestSVG />
+        <span>{{ repository.prs }}</span>
+      </span>
+    </p>
+  </div>
+</template>
+
+<script>
+import PullRequestSVG from '@/assets/svg/git-pull-request.svg'
+import StarFilledSVG from '@/assets/svg/star-fill.svg'
+import CircleFillSVG from '@/assets/svg/circle-fill.svg'
+
+export default {
+  props: {
+    repository: {
+      type: Object,
+    },
+  },
+  components: {
+    PullRequestSVG,
+    StarFilledSVG,
+    CircleFillSVG,
+  },
+}
+</script>
+
+<style scoped>
+.github-repo-card {
+  padding: 18px 28px;
+  background-color: var(--bg-color-lighter, var(--color-black-800));
+  border-radius: var(--radius-default);
+  color: var(--text-color-default);
+  flex: 1 1 220px;
+}
+
+.github-repo-card + .github-repo-card {
+  margin-left: 10px;
+}
+
+.github-repo-card__title {
+  font-family: var(--font-sans);
+  font-size: var(--text-lg);
+}
+
+.github-repo-card__title a {
+  text-decoration: none;
+  color: var(--color-primary);
+}
+
+.github-repo-card__title a:hover {
+  text-decoration: underline;
+}
+
+.github-repo-card__title a:active,
+.github-repo-card__title a:focus {
+  outline: 2px dashed var(--color-primary);
+}
+
+.github-repo-card__description {
+  color: var(--color-gray-300);
+}
+
+.github-repo-details {
+  margin-top: 18px;
+  color: var(--text-color-default);
+}
+
+.github-repo-details span {
+  font-weight: 700;
+  color: inherit;
+}
+
+.github-repo-details span + span {
+  margin-left: 32px;
+}
+
+.github-repo-language svg {
+  height: 12px;
+  width: 12px;
+}
+
+.github-repo-details svg {
+  vertical-align: inherit;
+  margin-right: 5px;
+}
+
+body[data-theme='light'] .github-repo-card {
+  border: 1px solid rgba(0, 0, 0, 0.1);
+}
+
+body[data-theme='light'] .github-repo-card__description {
+  color: var(--color-gray-500);
+}
+
+@media screen and (max-width: 425px) {
+  .github-repo-card + .github-repo-card {
+    margin-left: 0;
+    margin-top: 32px;
+  }
+}
+</style>
