d5c27c266d99f13e3563a52a7847a9fd406affbc
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2021-03-30 15:21:11 -0300

    feat: add lazy loading image component

---
+++ src/components/ui/ImgLazy.vue
@@ -0,0 +1,51 @@
+<template functional>
+  <img
+    :data-src="props.src"
+    :data-srcset="props.srcset"
+    :alt="props.alt"
+    :class="{ lozad: true, [props.className]: true }"
+    :width="props.width"
+    :height="props.height"
+    :style="`background-color: ${props.bgColor};`"
+  />
+</template>
+
+<script>
+export default {
+  name: 'ImageLazy',
+  props: {
+    src: {
+      type: String,
+      required: true,
+    },
+    srcset: {
+      type: String,
+      default: null,
+    },
+    alt: {
+      type: String,
+      default: '',
+    },
+    className: {
+      type: String,
+      default: '',
+    },
+    width: {
+      type: String,
+    },
+    height: {
+      type: String,
+    },
+    bgColor: {
+      type: String,
+      default: 'hsl(214, 9%, 55%)',
+    },
+  },
+}
+</script>
+
+<style scoped>
+.lozad {
+  object-fit: cover;
+}
+</style>
