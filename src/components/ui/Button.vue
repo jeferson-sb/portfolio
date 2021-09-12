2e657aec8511aac1cb2e798ce5fcebdd24e93aab
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2021-09-11 22:18:32 -0300

    chore: add circle button prop

--- src/components/ui/Button.vue
+++ src/components/ui/Button.vue
@@ -33,6 +33,9 @@ export default {
     full: {
       type: Boolean,
     },
+    circle: {
+      type: Boolean,
+    },
   },
   emits: ['click'],
   computed: {
@@ -46,6 +49,7 @@ export default {
         'button--squared': this.squared,
         'button--full': this.full,
         'button--icon': Boolean(this.$slots.icon),
+        'button--rounded-full': this.circle,
       }
     },
     isAnchor() {
@@ -61,17 +65,18 @@ export default {
   --button-text-color: var(--color-default-white);
   --button-shadow-color: hsla(187, 78%, 46%, 0.366);
   --button-fw: 400;
+  --button-radius: var(--radius-default);
 
   display: inline-block;
   text-align: center;
-  padding: 8px 16px;
+  padding: 0.5rem 1rem;
   max-width: fit-content;
   color: var(--button-text-color);
   background-color: var(--button-bg-color);
   font-weight: var(--button-fw);
   font-size: var(--button-font-size, inherit);
   transition: filter 0.3s ease-in-out;
-  border-radius: var(--radius-default);
+  border-radius: var(--button-radius);
 }
 
 .button:hover {
@@ -125,8 +130,18 @@ export default {
   margin-right: 4px;
 }
 
+.button--rounded-full {
+  --button-radius: 9999px;
+
+  padding: 0.75rem;
+}
+
+.button--rounded-full.button--icon > * {
+  margin: 0;
+}
+
 .button--squared {
-  border-radius: 0;
+  --button-radius: 0;
 }
 
 .button--full {
