45c1e278c450c5408d915a78cc9d850f2b5c41aa
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2022-03-02 11:07:23 -0300

    feat: add share button component

---
+++ src/components/ui/ShareButton.vue
@@ -0,0 +1,54 @@
+<template>
+  <Button variant="outline" @click="share" circle>
+    <template v-slot:icon>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <title>Share</title>
+        <circle cx="18" cy="5" r="3" />
+        <circle cx="6" cy="12" r="3" />
+        <circle cx="18" cy="19" r="3" />
+        <line x1="8.59" y1="13.51" x2="15.42" y2="17.49" />
+        <line x1="15.41" y1="6.51" x2="8.59" y2="10.49" />
+      </svg>
+    </template>
+  </Button>
+</template>
+
+<script>
+import Button from '@/components/ui/Button.vue'
+
+export default {
+  props: {
+    link: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+    },
+    title: {
+      type: String,
+      default: document.title,
+    },
+  },
+  components: { Button },
+  methods: {
+    async share() {
+      await window.navigator.share({
+        title: this.title,
+        text: this.text,
+        url: this.link,
+      })
+    },
+  },
+}
+</script>
