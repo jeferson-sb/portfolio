f9d4607541beff32b07c30ee40c3a655ec931e07
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2022-05-18 23:10:31 -0300

    feat: add homepage

---
+++ src/pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <main id="main-content">
+    <HeroSection />
+    <PostsList />
+    <ProjectsGrid />
+    <GithubProjects />
+  </main>
+</template>
+
+<script setup>
+import { useRoute } from 'vue-router'
+import config from '@/config/siteconfig.json'
+
+const { siteName: title } = config
+const { description } = config
+
+const route = useRoute()
+
+useHead({
+  title,
+  meta: [
+    {
+      name: 'description',
+      content: description,
+    },
+    { property: 'og:type', content: 'website' },
+    { property: 'og:title', content: title },
+    { property: 'og:description', content: description },
+    { property: 'og:url', content: `${config.siteUrl}${route.path}` },
+    {
+      property: 'twitter:title',
+      content: title,
+    },
+    {
+      property: 'twitter:description',
+      content: description,
+    },
+  ],
+})
+</script>
