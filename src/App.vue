f1841295cf828bbd04666353f00f25ef694e20ca
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2022-05-18 23:01:00 -0300

    feat: add App.vue template

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { useHead } from '@vueuse/head'
+import config from '@/config/siteconfig.json'
+
+useHead({
+  title: config.siteName,
+  meta: [{ name: 'description', content: config.description }],
+})
+</script>
+
+<template>
+  <router-view />
+</template>
