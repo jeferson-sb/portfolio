b4121891440097d073700303f9ad61be3f1ad383
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2022-05-18 23:09:09 -0300

    feat: add component svgs

---
+++ src/components/svg/BinaryTextSVG.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg height="170" width="200" class="binary-text-svg">
+    <text x="10" y="20" style="fill: currentColor">
+      <tspan x="10" y="45">0 1 1 0 1 0 0 0</tspan>
+      <tspan x="10" y="75">0 1 1 0 0 1 0 1</tspan>
+      <tspan x="10" y="105">0 1 1 0 1 1 0 0</tspan>
+      <tspan x="10" y="135">0 1 1 0 1 1 0 0</tspan>
+      <tspan x="10" y="165">0 1 1 0 1 1 1 1</tspan>
+    </text>
+  </svg>
+</template>
