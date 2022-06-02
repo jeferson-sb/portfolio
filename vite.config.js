627ed8bc1bac44e961d79084d4ef8efd7fac1e25
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2022-06-01 23:04:03 -0300

    chore: add shiki

--- vite.config.js
+++ vite.config.js
@@ -10,8 +10,9 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Markdown from 'vite-plugin-md'
 import { VitePWA } from 'vite-plugin-pwa'
 import Vql from 'vite-plugin-vue-gql'
-import Prism from 'markdown-it-prism'
+import Shiki from 'markdown-it-shiki'
 import MarkdownItAttrs from 'markdown-it-attrs'
+import MarkdownItAnchor from 'markdown-it-anchor'
 import matter from 'gray-matter'
 
 export default defineConfig({
@@ -62,10 +63,21 @@ export default defineConfig({
       wrapperClasses: 'prose',
       headEnabled: true,
       markdownItSetup(md) {
-        md.use(Prism, {
-          plugins: ['line-highlight', 'line-numbers'],
+        md.use(Shiki, {
+          theme: 'one-dark-pro',
         })
         md.use(MarkdownItAttrs)
+        md.use(MarkdownItAnchor, {
+          permalink: MarkdownItAnchor.permalink.linkInsideHeader({
+            symbol: `
+              <span class="sr-only">Jump to heading</span>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1" />
+              </svg>
+            `,
+            placement: 'before',
+          }),
+        })
       },
     }),
 
