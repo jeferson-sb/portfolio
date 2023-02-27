1e96cef6d3c890ea01e40e1fde6027cbd4fb50d3
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2023-02-27 10:36:14 -0300

    feat: add CustomList component

---
+++ src/components/article/CustomList.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps(['marker'])
+</script>
+
+<template>
+  <div class="custom-list">
+    <slot />
+  </div>
+</template>
+
+<style>
+.custom-list ul li::marker {
+  content: v-bind(marker);
+}
+
+.custom-list ul li {
+  padding-inline-start: 0.4em;
+}
+</style>
