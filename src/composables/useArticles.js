8c916fd1b69b7279db58b6f1b815c3fe18a2e546
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2023-11-11 01:27:27 -0300

    feat: add articles composable

---
+++ src/composables/useArticles.js
@@ -0,0 +1,40 @@
+import { computed } from 'vue'
+import { useRouter } from 'vue-router'
+
+export function useArticles(options) {
+  const router = useRouter()
+  const tag = options?.tag
+
+  const articles = computed(() => {
+    return router.getRoutes()
+      .filter((route) => {
+        const isArticle = route.path.startsWith('/articles/') && route.name
+        const tags = route.meta?.frontmatter?.tags
+
+        if (tag) {
+          const normalizedTags = tags ? tags.toLowerCase() : ''
+          const matchTag = normalizedTags.indexOf(tag.toLowerCase()) !== -1
+
+          return isArticle && matchTag
+        }
+
+        return isArticle
+      })
+      .sort((a, b) =>
+        a.meta.frontmatter.published_at < b.meta.frontmatter.published_at ? 1 : -1
+      )
+      .map((article) => ({
+        id: article.meta?.frontmatter?.id,
+        articleId: article.name,
+        title: article.meta?.frontmatter?.title,
+        date: article.meta?.frontmatter?.published_at,
+        path: article.path,
+        excerpt: article.meta?.frontmatter?.excerpt,
+        tags: article.meta?.frontmatter?.tags,
+      }))
+  })
+
+  if (tag && !articles.value.length) router.push('/404')
+
+  return articles
+}
