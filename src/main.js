ac966a47a609c797947205d20739593cc94a67d5
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2023-11-11 22:35:17 -0300

    chore: add import to utility layer

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import App from './App.vue'
 import '@/assets/styles/reset.css'
 import '@/assets/styles/global.css'
 import '@/assets/styles/markdown.css'
+import '@/assets/styles/utility.css'
 
 const routes = setupLayouts(generatedRoutes)
 
