6bd38544a20546514b9667c63596fd53ec9cc609
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2023-11-12 13:33:18 -0300

    fix: temporarily disable caching

--- vite.config.js
+++ vite.config.js
@@ -87,6 +87,7 @@ export default defineConfig({
         globPatterns: ['**/*{js,css,html,txt,ico,png,svg}'],
         cleanupOutdatedCaches: true,
       },
+      disable: true // temporarily 
     }),
   ],
   ssgOptions: {
